0ee7a993770c4942f68eeff84a4866ddbe498288
Sandra <sandra.alpha2010@gmail.com>
2020-04-24 21:50:45 -0700

    created question and answer generator function, leaderboardAppear function, index variable for array, created array of objects to hold question and answers, created variables to grab classes from html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,60 @@
 //creating variables to grab classes
-
-
+let questionE1 = document.querySelector(".question");
+let questionOneE1 = document.querySelector(".question-one");
+let questionTwoE1 = document.querySelector(".question-two");
+let questionThreeE1 = document.querySelector(".question-three");
+let questionTimerE1 = document.querySelector(".timer");
+let submitButton = document.querySelector(".button");
 
 
 //array of objects, array of questions and answers
 let computerChoices = [
     {
-
+        q: "",
+        a1: "",
+        a2: "",
+        a3: ""
     },
-
     {
-
+        q: "",
+        a1: "",
+        a2: "",
+        a3: ""
+    },
+    {
+        q: "",
+        a1: "",
+        a2: "",
+        a3: ""
     },
 
 ];
 
-//timer var
+//variable for timer, game starts with 60 secs
 let timer = 60;
+
+//variable for index of current question
+let computerChoicesIndex = 0;
+
+
+//function to generate question and answers
+function currentQa(){
+    //if there's another question and set of answers then the next set appears
+    if (computerChoicesIndex < computerChoices.length){
+        questionE1.innerHTML = computerChoices[computerChoicesIndex].q;
+        questionOneE1.innerHTML = computerChoices[computerChoicesIndex].a1;
+        questionOneE1.innerHTML = computerChoices[computerChoicesIndex].a2;
+        questionOneE1.innerHTML = computerChoices[computerChoicesIndex].a3;
+    }
+    //if there aren't any left, leaderboard screen appears
+    else {
+        leaderboardAppear();
+    }
+}
+
+
+//function to change the p element with a class of button into a bootstrap button
+function leaderboardAppear(){
+    
+
+}
