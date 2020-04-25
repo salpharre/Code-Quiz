48f79fdf878253e940b77f68277e0b614d7a3326
Sandra <sandra.alpha2010@gmail.com>
2020-04-24 19:39:44 -0700

    started skeleton of script and added comments to script js file

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//creating variables to grab classes
+
+
+
+
+//array of objects, array of questions and answers
+let computerChoices = [
+    {
+
+    },
+
+    {
+
+    },
+
+];
+
+//timer var
+let timer = 60;
